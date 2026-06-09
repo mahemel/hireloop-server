44760eba6d995b46df7432106fba044794e61a20
mahemel <mahbubhasanhemel@gmail.com>
2026-06-10 00:19:12 +0600

    api updated

--- index.js
+++ index.js
@@ -31,101 +31,135 @@ async function run() {
         const database = client.db(process.env.DB_NAME);
         const jobsCollection = database.collection("jobs");
         const companyCollection = database.collection("companyCollection");
-        const applicationCollection = database.collection("applicationCollection");
+        const usersCollection = database.collection("user");
+        const applicationsCollection = database.collection("applications");
+        const planCollection = database.collection('plans');
+        const subscriptionCollection = database.collection('subscriptions');
 
+        app.get('/api/users', async (req, res) => {
 
-        app.post('/api/jobs', async (req, res) => {
-            const job = req.body;
-
-            const newJob = {
-                ...job,
-                createdAt: new Date()
-            }
-
-            const result = await jobsCollection.insertOne(newJob);
-
-            res.json(result)
-
+            const cursor = usersCollection.find().skip(6);
+            const result = await cursor.toArray();
+            res.send(result);
         })
 
         app.get('/api/jobs', async (req, res) => {
             const query = {};
-
             if (req.query.companyId) {
-                query.companyId = req.query.companyId
+                query.companyId = req.query.companyId;
             }
             if (req.query.status) {
-                query.status = req.query.status
+                query.status = req.query.status;
             }
-
-            const cursor = jobsCollection.find(query);
+            const cursor = jobCollection.find(query);
             const result = await cursor.toArray();
-
-            res.json(result)
+            res.send(result);
         })
+
         app.get('/api/jobs/:id', async (req, res) => {
             const id = req.params.id;
             const query = {
                 _id: new ObjectId(id)
             }
-
-            const result = await jobsCollection.findOne(query);
-
-            res.json(result)
+            const result = await jobCollection.findOne(query);
+            res.send(result);
         })
 
-        //application related apis
+        app.post('/api/jobs', async (req, res) => {
+            const job = req.body;
+            const newJob = {
+                ...job,
+                createdAt: new Date()
+            }
+            const result = await jobCollection.insertOne(newJob);
+            res.send(result);
+        })
+
+        // application related apis
         app.get('/api/applications', async (req, res) => {
             const query = {};
-
             if (req.query.applicantId) {
                 query.applicantId = req.query.applicantId;
             }
             if (req.query.jobId) {
                 query.jobId = req.query.jobId;
             }
-
-            const cursor = applicationCollection.find(query);
+            const cursor = applicationsCollection.find(query);
             const result = await cursor.toArray();
-
-            res.json(result)
+            res.send(result);
         })
+
         app.post('/api/applications', async (req, res) => {
             const application = req.body;
-
             const newApplication = {
                 ...application,
                 createdAt: new Date()
             }
+            const result = await applicationsCollection.insertOne(newApplication);
+            res.send(result);
+        })
 
-            const result = await applicationCollection.insertOne(newApplication);
-            res.json(result)
+        // company related apis
+        app.get('/api/companies', async (req, res) => {
+            const cursor = companyCollection.find().skip(4);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
 
+
+        app.get('/api/my/companies', async (req, res) => {
+            const query = {};
+            if (req.query.recruiterId) {
+                query.recruiterId = req.query.recruiterId;
+            }
+            const result = await companyCollection.findOne(query);
+
+            res.send(result || {});
         })
 
         app.post('/api/companies', async (req, res) => {
             const company = req.body;
-
             const newCompany = {
                 ...company,
                 createdAt: new Date()
             }
             const result = await companyCollection.insertOne(newCompany);
-
-            res.json(result)
+            res.send(result);
         })
 
-        app.get('/api/my/companies', async (req, res) => {
-            const query = {};
+        // plans 
+        app.get('/api/plans', async (req, res) => {
+            const query = {}
+            if (req.query.plan_id) {
+                query.id = req.query.plan_id
+            }
+            const plan = await planCollection.findOne(query);
+            res.send(plan)
+        })
 
-            if (req.query.recruiterId) {
-                query.recruiterId = req.query.recruiterId
+        // subscription 
+        app.post('/api/subscriptions', async (req, res) => {
+            const data = req.body;
+            const subsInfo = {
+                ...data,
+                createdAt: new Date()
             }
 
-            const result = await companyCollection.findOne(query);
-            console.log(result)
-            res.json(result)
+            const result = await subscriptionCollection.insertOne(subsInfo);
+
+            // update the user plan information
+            const filter = { email: data.email };
+            // update the value of the 'quantity' field to 5
+            const updateDocument = {
+                $set: {
+                    plan: data.planId,
+                },
+            };
+
+            const updateResult = await usersCollection.updateOne(filter, updateDocument);
+            res.send(updateResult)
         })
+
 
     } finally {
         // Ensures that the client will close when you finish/error
