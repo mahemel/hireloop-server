cdeda72866a6fd8676b9c1e5495f1867968d2f1e
mahemel <mahbubhasanhemel@gmail.com>
2026-06-10 04:18:17 +0600

    plans, subscription, user data fetch api added

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express')
-const cors = require('cors')
+const cors = require('cors');
 const app = express()
 const port = 5001
-require('dotenv').config()
+require('dotenv').config();
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -13,10 +15,10 @@ app.get('/', (req, res) => {
 
 
 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
 const uri = process.env.MONGODB_URI;
 
-
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
@@ -27,10 +29,13 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
 
-        const database = client.db(process.env.DB_NAME);
-        const jobsCollection = database.collection("jobs");
-        const companyCollection = database.collection("companyCollection");
+
+        const database = client.db("hireloop_db");
+        const jobCollection = database.collection("jobs");
+        const companyCollection = database.collection("companies");
         const usersCollection = database.collection("user");
         const applicationsCollection = database.collection("applications");
         const planCollection = database.collection('plans');
@@ -40,6 +45,14 @@ async function run() {
 
             const cursor = usersCollection.find().skip(6);
             const result = await cursor.toArray();
+            res.send(result);
+        })
+        app.get('/api/user/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {
+                _id: new ObjectId(id)
+            }
+            const result = await usersCollection.findOne(query);
             res.send(result);
         })
 
@@ -160,13 +173,18 @@ async function run() {
             res.send(updateResult)
         })
 
-
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        //await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
+
+
+
 
 
 app.listen(port, () => {
