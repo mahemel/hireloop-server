9bd04ca31279b43dedbb632a0539909ec65ff4a4
mahemel <mahbubhasanhemel@gmail.com>
2026-06-09 20:19:47 +0600

    updated for deploy

--- index.js
+++ index.js
@@ -97,8 +97,6 @@ async function run() {
             res.json(result)
         })
 
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         //await client.close();
